2c64cefe5039a591f83cf654dee5bd634e93447d
Michael Bestas <mkbestas@lineageos.org>
2019-01-07 22:27:39 +0200

    Mi8937: Hex edit camera blobs to use /data/vendor/qcam
    (Partial commit)
    
    * Modify OSS HAL to use this path.
    
    for LIB in $(grep -lr "/data/misc/camera" .); do
        sed -i "s|/data/misc/camera|/data/vendor/qcam|g" "$LIB" || true
    done
    
    Change-Id: I220188d09b9da77a284ce40ac63d50c9054e8ba0

--- extract-files.sh
+++ extract-files.sh
@@ -21,6 +21,26 @@ function blob_fixup() {
             ;;
         vendor/lib/libmmcamera2_sensor_modules.so)
             sed -i 's|/system/etc/camera/|/vendor/etc/camera/|g' "${2}"
+            sed -i 's|data/misc/camera|data/vendor/qcam|g' "${2}"
+            ;;
+        vendor/lib/libmmcamera_tintless_bg_pca_algo.so \
+        |vendor/lib/libmmcamera_pdafcamif.so \
+        |vendor/lib/libmmcamera2_stats_modules.so \
+        |vendor/lib/libmmcamera2_dcrf.so \
+        |vendor/lib/libmmcamera_imglib.so \
+        |vendor/lib/libmmcamera_dbg.so \
+        |vendor/lib/libmmcamera2_stats_algorithm.so \
+        |vendor/lib/libmmcamera2_mct.so \
+        |vendor/lib/libmmcamera_tuning.so \
+        |vendor/lib/libmmcamera_tintless_algo.so \
+        |vendor/lib/libmmcamera2_iface_modules.so \
+        |vendor/lib/libmmcamera2_q3a_core.so \
+        |vendor/lib/libmmcamera2_pproc_modules.so \
+        |vendor/lib/libmmcamera2_imglib_modules.so \
+        |vendor/lib/libmmcamera2_cpp_module.so \
+        |vendor/lib/libmmcamera_pdaf.so \
+        |vendor/bin/wingtech_mm-qcamera-daemon)
+            sed -i 's|data/misc/camera|data/vendor/qcam|g' "${2}"
             ;;
     esac
 }
