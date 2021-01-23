152be9ba0200fefa18b5b0e025227344aed784ea
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-23 10:42:17 +0100

    Mi8937: Patch goodix libs
    * Remove useless shared libs dlopening
    
    Change-Id: I1b8028c926774d52899f4eaec4d499a104963719

--- extract-files.sh
+++ extract-files.sh
@@ -13,6 +13,17 @@ function blob_fixup() {
                 "${PATCHELF}" --add-needed "libshims_binder.so" "${2}"
             fi
             ;;
+        vendor/lib64/hw/gf_fingerprint.default.so \
+        |vendor/lib64/libgoodixfingerprintd_binder.so \
+        |vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so \
+        |vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
+            "${PATCHELF}" --remove-needed "libbacktrace.so" "${2}"
+            "${PATCHELF}" --remove-needed "libkeystore_binder.so" "${2}"
+            "${PATCHELF}" --remove-needed "libkeymaster_messages.so" "${2}"
+            "${PATCHELF}" --remove-needed "libsoftkeymaster.so" "${2}"
+            "${PATCHELF}" --remove-needed "libsoftkeymasterdevice.so" "${2}"
+            "${PATCHELF}" --remove-needed "libunwind.so" "${2}"
+            ;;
     esac
 }
 
