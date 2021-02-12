c32e537da8512b6e5cc06687593c13caf19da4ff
ZeeLog <Danjel.Zammit@gmail.com>
2021-02-12 09:18:38 +0100

    Mi8937: legacy goodix: Drop libunwind & libbacktrace deps
    Change-Id: I29881e2633746d4b810861e0a02cc1d035b1b053

--- extract-files.sh
+++ extract-files.sh
@@ -51,6 +51,8 @@ function blob_fixup() {
             ;;
         # Fingerprint (Legacy Goodix)
         vendor/overlayfs/*/bin/gx_fpcmd|vendor/overlayfs/*/bin/gx_fpd)
+            "${PATCHELF}" --remove-needed "libbacktrace.so" "${2}"
+            "${PATCHELF}" --remove-needed "libunwind.so" "${2}"
             if ! "${PATCHELF}" --print-needed "${2}" | grep "liblog.so" > /dev/null; then
                 "${PATCHELF}" --add-needed "liblog.so" "${2}"
             fi
