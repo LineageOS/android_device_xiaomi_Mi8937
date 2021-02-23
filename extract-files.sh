9db220d9059394c6a9a9ba3ec681085f3114c8ea
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-23 18:38:49 +0100

    Mi8937: Use patchelf v0.8 for goodix blobs
    Change-Id: I8f8de3517f88f82f1a16f94a134246e7a58b4820

--- extract-files.sh
+++ extract-files.sh
@@ -17,12 +17,12 @@ function blob_fixup() {
         |vendor/lib64/libgoodixfingerprintd_binder.so \
         |vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so \
         |vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
-            "${PATCHELF}" --remove-needed "libbacktrace.so" "${2}"
-            "${PATCHELF}" --remove-needed "libkeystore_binder.so" "${2}"
-            "${PATCHELF}" --remove-needed "libkeymaster_messages.so" "${2}"
-            "${PATCHELF}" --remove-needed "libsoftkeymaster.so" "${2}"
-            "${PATCHELF}" --remove-needed "libsoftkeymasterdevice.so" "${2}"
-            "${PATCHELF}" --remove-needed "libunwind.so" "${2}"
+            "${PATCHELF_0_8}" --remove-needed "libbacktrace.so" "${2}"
+            "${PATCHELF_0_8}" --remove-needed "libkeystore_binder.so" "${2}"
+            "${PATCHELF_0_8}" --remove-needed "libkeymaster_messages.so" "${2}"
+            "${PATCHELF_0_8}" --remove-needed "libsoftkeymaster.so" "${2}"
+            "${PATCHELF_0_8}" --remove-needed "libsoftkeymasterdevice.so" "${2}"
+            "${PATCHELF_0_8}" --remove-needed "libunwind.so" "${2}"
             ;;
     esac
 }
