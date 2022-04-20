f32372414a705fdf772d8a4aa79132a4a30dbc50
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-04-20 21:53:54 +0800

    ulysse: Move FP permission to fingerprint SKU

--- device.mk
+++ device.mk
@@ -28,9 +28,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_fingerprint/android.hardware.fingerprint.xml
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/blank.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/blank.xml
-
 # Audio
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc/)
