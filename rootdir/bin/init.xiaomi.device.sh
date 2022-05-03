dbc0e869ce0e908085b73e9565b793d2e7d28b86
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-03 20:08:07 +0800

    Mi8937: biometrics: Wire up both FP HIDLs

--- rootdir/bin/init.xiaomi.device.sh
+++ rootdir/bin/init.xiaomi.device.sh
@@ -18,18 +18,24 @@ case "$(cat /sys/firmware/devicetree/base/model)" in
 		setprop ro.vendor.xiaomi.series rova
 		# Audio
 		set_acdb_path_props rova
+		# Fingerprint
+		setprop ro.vendor.fingerprint.supported 0
 		;;
 	"Qualcomm Technologies, Inc. MSM8917 QRD SKU5")
 		setprop ro.vendor.xiaomi.device riva
 		setprop ro.vendor.xiaomi.series rova
 		# Audio
 		set_acdb_path_props rova
+		# Fingerprint
+		setprop ro.vendor.fingerprint.supported 0
 		;;
 	"Qualcomm Technologies, Inc. MSM8917-PMI8937 MTP")
 		setprop ro.vendor.xiaomi.device ugglite
 		setprop ro.vendor.xiaomi.series ulysse
 		# Audio
 		set_acdb_path_props ulysse
+		# Fingerprint
+		setprop ro.vendor.fingerprint.supported 0
 		;;
 	"Qualcomm Technologies, Inc. MSM8940-PMI8937 MTP")
 		setprop ro.vendor.xiaomi.device ugg
@@ -41,6 +47,8 @@ case "$(cat /sys/firmware/devicetree/base/model)" in
 		setprop persist.s5k3p8sp.flash.light 300
 		setprop persist.ov16885.flash.low 290
 		setprop persist.ov16885.flash.light 275
+		# Fingerprint
+		setprop ro.vendor.fingerprint.supported 2
 		;;
 	"Qualcomm Technologies, Inc. MSM8937-PMI8950 QRD SKU1")
 		# Device Info
@@ -48,6 +56,12 @@ case "$(cat /sys/firmware/devicetree/base/model)" in
 		setprop ro.vendor.xiaomi.series landtoni
 		# Audio
 		set_acdb_path_props land
+		# Fingerprint
+		if grep -E "S88537AC1|S88537EC1" /proc/cmdline ; then
+			setprop ro.vendor.fingerprint.supported 0
+		else
+			setprop ro.vendor.fingerprint.supported 1
+		fi
 		;;
 	"Qualcomm Technologies, Inc. MSM8940-PMI8950 QRD SKU7")
 		# Device Info
@@ -55,11 +69,16 @@ case "$(cat /sys/firmware/devicetree/base/model)" in
 		setprop ro.vendor.xiaomi.series landtoni
 		# Audio
 		set_acdb_path_props santoni
+		# Fingerprint
+		setprop vendor.fingerprint.disable_notify_cancel_hack 1
+		setprop ro.vendor.fingerprint.supported 1
 		;;
 	"Qualcomm Technologies, Inc. MSM8937-PMI8950 MTP")
 		setprop ro.vendor.xiaomi.device prada
 		# Audio
 		set_acdb_path_props prada
+		# Fingerprint
+		setprop ro.vendor.fingerprint.supported 1
 		;;
 esac
 
