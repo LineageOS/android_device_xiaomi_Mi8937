7d5960475bd5edd5606b5fbdad0150c708e7c60c
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-04 02:50:28 +0800

    Mi8937: Shim lib_fpc_tac_shared.so with libshims_binder
    Co-authored-by: Albert I <kras@raphielgang.org>

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/lib_fpc_tac_shared.so)
+            if ! "${PATCHELF}" --print-needed "${2}" | grep "libshims_binder.so" >/dev/null; then
+                "${PATCHELF}" --add-needed "libshims_binder.so" "${2}"
+            fi
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
