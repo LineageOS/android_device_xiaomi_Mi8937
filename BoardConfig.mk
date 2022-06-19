6ed5136bdb249e61d5731283183fa131f6502dde
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-20 03:19:01 +0800

    Mi8937: Commonize kernel cmdline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,6 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_mi8937
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_mi8937
 
 # Kernel
-BOARD_KERNEL_CMDLINE += earlycon=msm_hsl_uart,0x78b000
 TARGET_KERNEL_CONFIG := mi8937_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
 
