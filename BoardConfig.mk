f309728c7d2bf5a8cd6cf64f99a9c5d474d553c9
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-26 02:26:02 +0800

    Mi8937: Specify a non-existent dt directory to avoid using dt fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_mi8937
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_mi8937
 
 # Kernel
-BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/7824900.sdhci
+BOARD_KERNEL_CMDLINE += androidboot.android_dt_dir=/non-existent androidboot.boot_devices=soc/7824900.sdhci
 TARGET_KERNEL_CONFIG := mi8937_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
 
