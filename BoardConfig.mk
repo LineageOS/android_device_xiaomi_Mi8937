9f6016e02c2a6861c1eb84e235a9d3459060d05c
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-26 02:27:59 +0800

    Mi8937: Disable custom inode count calculation for SSI partitions

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,7 +62,7 @@ BOARD_MI8937_DYNPART_PARTITION_LIST := $(ALL_PARTITIONS)
 
 # Partitions - reserved size
 $(foreach p, $(call to-upper, $(SSI_PARTITIONS)), \
-    $(eval BOARD_$(p)IMAGE_EXTFS_INODE_COUNT := 6144))
+    $(eval BOARD_$(p)IMAGE_EXTFS_INODE_COUNT := -1))
 $(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
     $(eval BOARD_$(p)IMAGE_EXTFS_INODE_COUNT := 4096))
 
