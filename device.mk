15982e3d621f09a579dabbdd5f5b8b514dd5d7c2
me-cafebabe <me.cafebabe@gmail.com>
2023-03-05 20:28:26 +0800

    Mi8937: Copy hvdcp_opti to recovery
    * It's responsible for reporting correct battery capacity, at least.
    * The newly added charger_led service needs correct battery capacity
    * None of Mi8917 devices uses QCOM PMIC charger, Guard it for Mi8937 only
    
    Change-Id: I3a14e02f8b857a8007a34fbbe902949ead3373a3

--- device.mk
+++ device.mk
@@ -76,6 +76,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/blankfile:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/sensors/,$(TARGET_COPY_OUT_VENDOR)/etc/sensors/)
 
+# Recovery
+ifeq ($(PRODUCT_HARDWARE),Mi8937)
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/Mi8937/proprietary/vendor/bin/hvdcp_opti:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/hvdcp_opti
+endif
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.qcom_ramdisk \
