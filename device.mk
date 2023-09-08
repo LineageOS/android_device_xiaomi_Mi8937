02dec047d5aab5df485d7cd89ed960ff02cfd76d
me-cafebabe <me.cafebabe@gmail.com>
2023-09-09 01:56:00 +0800

    Mi8937: Move fingerprint blobs to odm partition
    Change-Id: Idda2b115575214137678d71f72c232e5d8312f6e

--- device.mk
+++ device.mk
@@ -76,6 +76,11 @@ PRODUCT_COPY_FILES += \
     $(foreach f, msm8917-sku5-snd-card_Button_Jack.kl msm8920-sku7-snd-card_Button_Jack.kl msm8952-sku1-snd-card_Button_Jack.kl, \
         $(LOCAL_PATH)/keylayout/msm8952-snd-card-mtp_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/$(f))
 
+# Placeholder
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/blankfile:$(TARGET_COPY_OUT_ODM)/bin/.placeholder \
+    $(LOCAL_PATH)/configs/blankfile:$(TARGET_COPY_OUT_ODM)/lib64/.placeholder
+
 # Recovery
 ifeq ($(PRODUCT_HARDWARE),Mi8937)
 PRODUCT_COPY_FILES += \
