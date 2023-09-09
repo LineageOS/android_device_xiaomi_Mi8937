1f5190c9061c335131f3bcac6b562067a114c44a
me-cafebabe <me.cafebabe@gmail.com>
2023-09-09 15:40:55 +0800

    Mi8937: Handle wingtech nougat camera blobs with overlayfs
    Change-Id: Ice3a4618882b28103903e1d4fe7e117a32b64a34

--- extract-files.sh
+++ extract-files.sh
@@ -9,44 +9,45 @@
 function blob_fixup() {
     case "${1}" in
         # Camera (Wingtech Nougat)
-        vendor/lib/libmmsw_platform.so|vendor/lib/libmmsw_detail_enhancement.so)
+        odm/overlayfs/*/lib/libmmsw_platform.so|odm/overlayfs/*/lib/libmmsw_detail_enhancement.so)
             "${PATCHELF}" --remove-needed "libbinder.so" "${2}"
             sed -i 's|libgui.so|libwui.so|g' "${2}"
             ;;
-        vendor/lib/libmmcamera2_sensor_modules.so)
-            sed -i 's|/system/etc/camera/|/vendor/etc/camera/|g' "${2}"
+        odm/overlayfs/*/lib/libmmcamera2_sensor_modules.so)
+            sed -i 's|/system/etc/camera/|////odm/etc/camera/|g' "${2}"
+            sed -i 's|/system/vendor/lib|////vendor/odm/lib|g' "${2}"
             sed -i 's|data/misc/camera|data/vendor/qcam|g' "${2}"
             ;;
-        vendor/lib/libmmcamera_tintless_bg_pca_algo.so \
-        |vendor/lib/libmmcamera_pdafcamif.so \
-        |vendor/lib/libmmcamera2_dcrf.so \
-        |vendor/lib/libmmcamera_imglib.so \
-        |vendor/lib/libmmcamera_dbg.so \
-        |vendor/lib/libmmcamera2_stats_algorithm.so \
-        |vendor/lib/libmmcamera2_mct.so \
-        |vendor/lib/libmmcamera_tuning.so \
-        |vendor/lib/libmmcamera_tintless_algo.so \
-        |vendor/lib/libmmcamera2_iface_modules.so \
-        |vendor/lib/libmmcamera2_q3a_core.so \
-        |vendor/lib/libmmcamera2_pproc_modules.so \
-        |vendor/lib/libmmcamera2_imglib_modules.so \
-        |vendor/lib/libmmcamera2_cpp_module.so \
-        |vendor/lib/libmmcamera_pdaf.so \
-        |vendor/bin/wingtech_mm-qcamera-daemon)
+        odm/overlayfs/*/lib/libmmcamera_tintless_bg_pca_algo.so \
+        |odm/overlayfs/*/lib/libmmcamera_pdafcamif.so \
+        |odm/overlayfs/*/lib/libmmcamera2_dcrf.so \
+        |odm/overlayfs/*/lib/libmmcamera_imglib.so \
+        |odm/overlayfs/*/lib/libmmcamera_dbg.so \
+        |odm/overlayfs/*/lib/libmmcamera2_stats_algorithm.so \
+        |odm/overlayfs/*/lib/libmmcamera2_mct.so \
+        |odm/overlayfs/*/lib/libmmcamera_tuning.so \
+        |odm/overlayfs/*/lib/libmmcamera_tintless_algo.so \
+        |odm/overlayfs/*/lib/libmmcamera2_iface_modules.so \
+        |odm/overlayfs/*/lib/libmmcamera2_q3a_core.so \
+        |odm/overlayfs/*/lib/libmmcamera2_pproc_modules.so \
+        |odm/overlayfs/*/lib/libmmcamera2_imglib_modules.so \
+        |odm/overlayfs/*/lib/libmmcamera2_cpp_module.so \
+        |odm/overlayfs/*/lib/libmmcamera_pdaf.so \
+        |odm/overlayfs/*/bin/mm-qcamera-daemon)
             sed -i 's|data/misc/camera|data/vendor/qcam|g' "${2}"
             ;;
-        vendor/lib/libmmcamera2_stats_modules.so)
+        odm/overlayfs/*/lib/libmmcamera2_stats_modules.so)
             sed -i 's|data/misc/camera|data/vendor/qcam|g' "${2}"
             sed -i 's|libgui.so|libwui.so|g' "${2}"
             "${PATCHELF}" --replace-needed "libandroid.so" "libshims_android.so" "${2}"
             ;;
-        vendor/lib/libmmcamera_ppeiscore.so)
+        odm/overlayfs/*/lib/libmmcamera_ppeiscore.so)
             sed -i 's|libgui.so|libwui.so|g' "${2}"
             if ! "${PATCHELF}" --print-needed "${2}" | grep "libshims_ui.so" >/dev/null; then
                 "${PATCHELF}" --add-needed "libshims_ui.so" "${2}"
             fi
             ;;
-        vendor/lib/libmpbase.so)
+        odm/overlayfs/*/lib/libmpbase.so)
             "${PATCHELF}" --replace-needed "libandroid.so" "libshims_android.so" "${2}"
             ;;
         # Fingerprint (Legacy Goodix)
