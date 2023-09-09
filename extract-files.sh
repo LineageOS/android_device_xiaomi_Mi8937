ad8aff7988430f1a337ea7e6eb531092141d9910
me-cafebabe <me.cafebabe@gmail.com>
2023-09-09 15:47:22 +0800

    Mi8937: extract-files: Simplify camera fixup logic
    Change-Id: I965eadad0197b6738bdb34ea57ab9a49d46ac998

--- extract-files.sh
+++ extract-files.sh
@@ -7,45 +7,31 @@
 #
 
 function blob_fixup() {
+    # Camera
+    if [[ "${1}" =~ ^odm/overlayfs/.*/lib/libmmcamera.*\.so$ ]]; then
+        sed -i 's|data/misc/camera|data/vendor/qcam|g;s|libgui.so|libwui.so|g' "${2}"
+    fi
+
     case "${1}" in
-        # Camera (Wingtech Nougat)
-        odm/overlayfs/*/lib/libmmsw_platform.so|odm/overlayfs/*/lib/libmmsw_detail_enhancement.so)
-            "${PATCHELF}" --remove-needed "libbinder.so" "${2}"
-            sed -i 's|libgui.so|libwui.so|g' "${2}"
+        # Camera
+        odm/overlayfs/*/bin/mm-qcamera-daemon)
+            sed -i 's|data/misc/camera|data/vendor/qcam|g' "${2}"
+            ;;
+        odm/overlayfs/*/lib/libmmcamera_ppeiscore.so)
+            if ! "${PATCHELF}" --print-needed "${2}" | grep "libshims_ui.so" > /dev/null; then
+                "${PATCHELF}" --add-needed "libshims_ui.so" "${2}"
+            fi
             ;;
         odm/overlayfs/*/lib/libmmcamera2_sensor_modules.so)
             sed -i 's|/system/etc/camera/|////odm/etc/camera/|g' "${2}"
             sed -i 's|/system/vendor/lib|////vendor/odm/lib|g' "${2}"
-            sed -i 's|data/misc/camera|data/vendor/qcam|g' "${2}"
-            ;;
-        odm/overlayfs/*/lib/libmmcamera_tintless_bg_pca_algo.so \
-        |odm/overlayfs/*/lib/libmmcamera_pdafcamif.so \
-        |odm/overlayfs/*/lib/libmmcamera2_dcrf.so \
-        |odm/overlayfs/*/lib/libmmcamera_imglib.so \
-        |odm/overlayfs/*/lib/libmmcamera_dbg.so \
-        |odm/overlayfs/*/lib/libmmcamera2_stats_algorithm.so \
-        |odm/overlayfs/*/lib/libmmcamera2_mct.so \
-        |odm/overlayfs/*/lib/libmmcamera_tuning.so \
-        |odm/overlayfs/*/lib/libmmcamera_tintless_algo.so \
-        |odm/overlayfs/*/lib/libmmcamera2_iface_modules.so \
-        |odm/overlayfs/*/lib/libmmcamera2_q3a_core.so \
-        |odm/overlayfs/*/lib/libmmcamera2_pproc_modules.so \
-        |odm/overlayfs/*/lib/libmmcamera2_imglib_modules.so \
-        |odm/overlayfs/*/lib/libmmcamera2_cpp_module.so \
-        |odm/overlayfs/*/lib/libmmcamera_pdaf.so \
-        |odm/overlayfs/*/bin/mm-qcamera-daemon)
-            sed -i 's|data/misc/camera|data/vendor/qcam|g' "${2}"
             ;;
         odm/overlayfs/*/lib/libmmcamera2_stats_modules.so)
-            sed -i 's|data/misc/camera|data/vendor/qcam|g' "${2}"
-            sed -i 's|libgui.so|libwui.so|g' "${2}"
             "${PATCHELF}" --replace-needed "libandroid.so" "libshims_android.so" "${2}"
             ;;
-        odm/overlayfs/*/lib/libmmcamera_ppeiscore.so)
+        odm/overlayfs/*/lib/libmmsw_platform.so|odm/overlayfs/*/lib/libmmsw_detail_enhancement.so)
+            "${PATCHELF}" --remove-needed "libbinder.so" "${2}"
             sed -i 's|libgui.so|libwui.so|g' "${2}"
-            if ! "${PATCHELF}" --print-needed "${2}" | grep "libshims_ui.so" >/dev/null; then
-                "${PATCHELF}" --add-needed "libshims_ui.so" "${2}"
-            fi
             ;;
         odm/overlayfs/*/lib/libmpbase.so)
             "${PATCHELF}" --replace-needed "libandroid.so" "libshims_android.so" "${2}"
