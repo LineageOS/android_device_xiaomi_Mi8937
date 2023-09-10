fb49f6b3960f00ff68bfa73d163898da98ad9d67
me-cafebabe <me.cafebabe@gmail.com>
2023-09-10 22:52:18 +0800

    Mi8937: extract-files: Fixup legacy goodix blobs using host patchelf
    * patchelf from prebuilts/extract-tools breaks these blobs somehow
    
    Change-Id: I989eb16682247c1b8b11f6337d01506d679ea756

--- extract-files.sh
+++ extract-files.sh
@@ -51,26 +51,26 @@ function blob_fixup() {
             ;;
         # Fingerprint (Legacy Goodix)
         odm/overlayfs/*/bin/gx_fpcmd|odm/overlayfs/*/bin/gx_fpd)
-            "${PATCHELF}" --remove-needed "libbacktrace.so" "${2}"
-            "${PATCHELF}" --remove-needed "libunwind.so" "${2}"
-            if ! "${PATCHELF}" --print-needed "${2}" | grep "libfakelogprint.so" > /dev/null; then
-                "${PATCHELF}" --add-needed "libfakelogprint.so" "${2}"
+            patchelf --remove-needed "libbacktrace.so" "${2}"
+            patchelf --remove-needed "libunwind.so" "${2}"
+            if ! patchelf --print-needed "${2}" | grep "libfakelogprint.so" > /dev/null; then
+                patchelf --add-needed "libfakelogprint.so" "${2}"
             fi
             ;;
         odm/overlayfs/*/lib64/libfpservice.so)
-            if ! "${PATCHELF}" --print-needed "${2}" | grep "libbinder_shim.so" > /dev/null; then
-                "${PATCHELF}" --add-needed "libbinder_shim.so" "${2}"
+            if ! patchelf --print-needed "${2}" | grep "libbinder_shim.so" > /dev/null; then
+                patchelf --add-needed "libbinder_shim.so" "${2}"
             fi
             ;;
         odm/overlayfs/*/lib64/hw/fingerprint.*_goodix.so)
             sed -i 's|libandroid_runtime.so|libshims_android.so\x00\x00|g' "${2}"
-            if ! "${PATCHELF}" --print-needed "${2}" | grep "libfakelogprint.so" > /dev/null; then
-                "${PATCHELF}" --add-needed "libfakelogprint.so" "${2}"
+            if ! patchelf --print-needed "${2}" | grep "libfakelogprint.so" > /dev/null; then
+                patchelf --add-needed "libfakelogprint.so" "${2}"
             fi
             ;;
         odm/overlayfs/*/lib64/hw/gxfingerprint.*.so)
-            if ! "${PATCHELF}" --print-needed "${2}" | grep "libfakelogprint.so" > /dev/null; then
-                "${PATCHELF}" --add-needed "libfakelogprint.so" "${2}"
+            if ! patchelf --print-needed "${2}" | grep "libfakelogprint.so" > /dev/null; then
+                patchelf --add-needed "libfakelogprint.so" "${2}"
             fi
             ;;
         # Fingerprint (ugg)
