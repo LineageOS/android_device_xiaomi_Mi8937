9927e5fbf96b3ea72e1d00661cced65ca4ea297d
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-15 01:47:41 +0800

    Mi8937: Introduce radio config wrapper
    * This lets us have radio.config@1.1 interface while the modem is
      only capable of radio.config@1.0
    * Main purpose is to fix RIL after U QPR2, as radio.config@1.0 is
      no longer supported. Switching to radio.config@1.1 directly results
      in broken mobile data switch (Workaround that in telephony framework
      is possible, this way we fix it on AOSP GSIs too)
    
    Change-Id: If9f64d3fc2bea54effd5cd6c98dce2f2426d3143

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,8 @@ TARGET_SCREEN_DENSITY := 280
 TARGET_FS_CONFIG_GEN += $(DEVICE_PATH)/config.fs
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Init
