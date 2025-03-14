d00daf8388f61c7bfca135b3e3e9642fb658a2c6
Yumi Yukimura <me.cafebabe@gmail.com>
2025-03-14 08:22:22 +0800

    Mi8937: Adjust inode count on odm and vendor partition
    * We've just moved a lot of blobs from odm to vendor
    
    Change-Id: I4072886080087e583c25555e8d9fc9827500c4fd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,8 +87,9 @@ BOARD_MI8937_DYNPART_PARTITION_LIST := $(ALL_PARTITIONS)
 # Partitions - reserved size
 $(foreach p, $(call to-upper, $(SSI_PARTITIONS)), \
     $(eval BOARD_$(p)IMAGE_EXTFS_INODE_COUNT := -1))
-$(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
-    $(eval BOARD_$(p)IMAGE_EXTFS_INODE_COUNT := 2048))
+
+BOARD_ODMIMAGE_EXTFS_INODE_COUNT := 512
+BOARD_VENDORIMAGE_EXTFS_INODE_COUNT := 4096
 
 $(foreach p, $(call to-upper, $(SSI_PARTITIONS)), \
     $(eval BOARD_$(p)IMAGE_PARTITION_RESERVED_SIZE := 83886080)) # 80 MB
